6a3271379c2b92830c7fcadcc981e66cd023bea1
Hrithik Chauhan <itzhrithan@gmail.com>
2020-01-04 19:01:13 +0530

    Updates

---
+++ precache-manifest.fb85ddb00fa8e6628d1052e00273ab24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c92e8cf5cd449922d4230f8368908e7",
+    "url": "/ReactJSHarryIpsum/index.html"
+  },
+  {
+    "revision": "a8df64b432f50ef42d7b",
+    "url": "/ReactJSHarryIpsum/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "742b2e9be43f3897dbba",
+    "url": "/ReactJSHarryIpsum/static/css/main.88288eb4.chunk.css"
+  },
+  {
+    "revision": "a8df64b432f50ef42d7b",
+    "url": "/ReactJSHarryIpsum/static/js/2.931a04da.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/ReactJSHarryIpsum/static/js/2.931a04da.chunk.js.LICENSE"
+  },
+  {
+    "revision": "742b2e9be43f3897dbba",
+    "url": "/ReactJSHarryIpsum/static/js/main.8235cc0d.chunk.js"
+  },
+  {
+    "revision": "df892112e2a544a4f50a",
+    "url": "/ReactJSHarryIpsum/static/js/runtime-main.fe70705b.js"
+  }
+]);
